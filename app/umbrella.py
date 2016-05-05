4ceff8d3713a9eee54bab7e2a06cacc39008d68f
Israel Santana <isantana@inerza.com>
2016-05-05 10:26:03 +0100

    Change behaviour of valid_genome variable
    The loop in get_base was inefficient

--- app/umbrella.py
+++ app/umbrella.py
@@ -5,13 +5,14 @@ def baseComparison(sec1, sec2):
         return False
 
 def get_base(sec):
-    check_valid_base(sec)
+    valid_genome = ['A', 'T', 'C', 'G']
+    check_valid_base(sec, valid_genome)
     result = dict()
-    for genome in sec:
+    for genome in valid_genome:
         result[genome] = sec.count(genome)
     return result
 
-def check_valid_base(sec):
+def check_valid_base(sec, valid_genome):
     for char in sec:
-        if char not in ['A', 'T', 'C', 'G']:
+        if char not in valid_genome:
             raise ValueError('Base not valid')
